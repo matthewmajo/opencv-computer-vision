b242982a8553734b551af704560c25c80f010013
matthewmajo <80959059+matthewmajo@users.noreply.github.com>
2021-07-11 19:12:08 +0100

    Update 1.py

--- liveproject/colorblender/1.py
+++ liveproject/colorblender/1.py
@@ -11,35 +11,29 @@ import cv2
 
 # Create a white canvas
 def createCanvas(color):
-    size = [640, 640]
-    canvas = np.ones((size[0], size[1], 3), dtype=np.uint8)
-    canvas = np.uint8(canvas * color)
+    size = [640, 640]   # Size of canvas 640x640 pixels
+    canvas = np.ones((size[0], size[1], 3), dtype=np.uint8) # Creates array of 1s with rgb value per pixel
+    canvas = np.uint8(canvas * color)   # Makes colour of canvas the same as input color
     return canvas
     
     
 
 # Display an image
 def displayImage(image, WindowName):
-    cv2.imshow(WindowName ,image)
+    cv2.imshow(WindowName, image)   # Shows an input image 
     
 
 # Load the color palette
-# Your code here
 palette_image = cv2.imread('C:/Users/matma/Documents/liveproject/colorblender/colorpalette/kHTKrqM.jpg')
-palette_resized = cv2.resize(palette_image, (640, 640),)
+# Reads color palette image
+palette_resized = cv2.resize(palette_image, (640, 640),) # Resizes palette to 640x640 pixels
 
 # Create canvas
-# Use the createCanvas function
-# you have written above
-# Your code here
-canvas = createCanvas([255,255,255])
+canvas = createCanvas([255,255,255]) # Creates a white canvas
 
 # Display the canvas and the 
 # color palette
-# Use the functions you have 
-# written above
-# Your code here
-display_canvas = displayImage(canvas, 'Canvas')
-display_palette = displayImage(palette_resized, 'Color Palete')
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+display_canvas = displayImage(canvas, 'Canvas') # Displays white canvas
+display_palette = displayImage(palette_resized, 'Color Palete') # Displays images indefinately
+cv2.waitKey(0)  # Shows images immediately
+cv2.destroyAllWindows() # Allows closing of image windows
